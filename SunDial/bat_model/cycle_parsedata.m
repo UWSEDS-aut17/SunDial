db84cd2478e8f51e799c6fc8db075a7fe472c185
Ryan Stoddard <rstodd@uw.edu>
2017-12-04 20:29:15 -0800

    finished parsing cycle data

--- SunDial/bat_model/cycle_parsedata.m
+++ SunDial/bat_model/cycle_parsedata.m
@@ -1,4 +1,5 @@
-%load('data/SOC_0%-60%_HalfC/PL03.mat')
+%{
+load('data/SOC_0%-60%_HalfC/PL03.mat')
 
 dataset = zeros(50*25,8);
 
@@ -36,3 +37,44 @@ end
 toc
 
 data2 = PL03{3,3};
+%}
+load('data/SOC_20%-80%_HalfC/PL21.mat')
+
+dataset = zeros(50*25,8);
+
+%rows: day[1] charge_rate[2] discharge_rate[3] lowSOC[4] highSOC[5] cyclenum[6] Cap%[7]
+%days = [1 21 36 55 90 104
+
+%
+tic
+idx0 = 1;
+idx1 = 1;
+cycle = 1;
+for jj = [5:14]
+    
+    data2 = PL21{jj,3};
+    idxs = find(data2{2:end,4} - data2{1:end-1,4} == 1);
+    cap = zeros(size(idxs));
+    for ii = 1:length(idxs)
+        cap(ii) = data2{idxs(ii),8};
+        
+    end
+    cap2 = diff(cap);
+    dataset(idx1:idx1+length(cap2)-1,1) = day(datetime(PL21{jj,2}) - datetime(PL21{2,2})); %fix year thing
+    dataset(idx1:idx1+length(cap2)-1,2) = 0.5; %always
+    dataset(idx1:idx1+length(cap2)-1,3) = 0.5;
+    dataset(idx1:idx1+length(cap2)-1,4) = 0;
+    dataset(idx1:idx1+length(cap2)-1,5) = 60;
+    dataset(idx1:idx1+length(cap2)-1,6) = cycle:cycle+length(cap2)-1;
+    dataset(idx1:idx1+length(cap2)-1,7) = cap2/cap2(idx0);
+    idx1 = idx1 + length(cap2)-1;
+    cycle = cycle + length(cap2)-1;
+    %delete outliers
+    
+    %days = day(datetime(PL03{jj,2}) - datetime(PL03{2,2}));
+end
+dataset(find(dataset(:,7) > 1.1),:) = [];
+dataset(find(dataset(:,7) < .5),:) = [];
+toc
+
+data2 = PL21{3,3};
