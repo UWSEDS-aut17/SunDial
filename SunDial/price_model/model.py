d75dbd7cd2e9202da5f97f58d1987a3adc09504b
Vardhman Mehta <mehtav@cs.washington.edu>
2017-12-07 20:50:53 -0800

    trying to make a python script with models

--- SunDial/price_model/model.py
+++ SunDial/price_model/model.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -11,42 +11,61 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
 from sklearn import svm
-from sklearn.metrics import confusion_matrix
-from sklearn.neural_network import MLPRegressor
+from sklearn import grid_search
+from sklearn.grid_search import GridSearchCV
 
 REGRESSORS = {
-				"SVM_rbf": svm.SVR(kernel='rbf', C=1000, gamma=0.001),
+				"SVM_rbf": svm.SVR(C=1000, gamma=0.001, kernel='rbf', epsilon=10)
 				# "Linear": LinearRegression(normalize=True),
 				# "KNN": KNeighborsRegressor(n_neighbors=10)
 			 }
 
 DATA_ITEMS = 25
+PRICE_DATA_FILENAME = "../data/sm_price/processed_price.csv"
 
 
-def model(X, y):
-	Xtrain, Xtest, ytrain, ytest = train_test_split(X, y)
+def get_train_test_data(df_price_frame):
+	df_price_frame = df_price_frame.dropna()
+	X_price_frame = df_price_frame.drop('lmp_value', axis=1).reset_index().drop('time', axis=1)
+	Y_price_frame = df_price_frame['lmp_value']
+	return train_test_split(X_price_frame, Y_price_frame, shuffle=False)
+
+def model(df_price_frame):
+	x_train, x_test, y_train, y_test = get_train_test_data(df_price_frame)
 	
 	for model_name, model in REGRESSORS.items():
-		print model_name
-		model.fit(Xtrain[:, 1:], ytrain)
-		ypred = model.predict(Xtest[:, 1:])
+		svr_reg = model.fit(x_train, y_train)
+		y_pred = svr_reg.fit(x_train, y_train).predict(x_test)
 
-		x_vals = Xtest[:, 0][:DATA_ITEMS]
-		y_vals = model.predict(Xtest[:, 1:])[:DATA_ITEMS]
+		# print 'Optimum parameters epsilon and kernel for SVR: ', svr_reg.best_params_
 
-		score = model.score(Xtest[:, 1:], ytest)
-		print score
+		print "The test score R2 for SVR: ", svr_reg.score(x_test, y_test)
 
-		plt.title(model_name)
-		plt.scatter(x_vals, list(ytest)[:DATA_ITEMS], color='b')
-		plt.scatter(x_vals, list(y_vals), color='g')
+		print("SVR mean squared error: %.2f"
+		      % np.mean((y_test - svr_reg.predict(x_test)) ** 2))
+
+		fig = plt.figure(figsize=(15,7))
+		plt.scatter(x_test.index, y_test, c='k', label='Observed')
+		plt.plot(x_test.index, y_pred, c='r', label='Predicted')
+		plt.xlabel('data')
+		plt.ylabel('lmp_value')
+		plt.title('Support Vector Regression')
+		plt.legend()
 		plt.show()
 
+		fig = plt.figure(figsize=(6,6))
+		plt.scatter(y_test, y_test, c='k')
+		plt.scatter(y_test, y_pred, c='r')
+		plt.xlabel('Observed Elec. Usage (kWh)')
+		plt.ylabel("Predicted Elec. Usage (kWh): $\hat{Y}_i$")
+		plt.title("Energy vs Predicted Energy: $Y_i$ vs $\hat{Y}_i$")
+		plt.show()
+
+
 def main():
-	price_data, headers = process_data()
-	y = price_data[:, 0]
-	x = price_data[:, 1:]
-	model(x, y)
+	price_frame = pd.read_csv(PRICE_DATA_FILENAME, index_col=0)
+	df_price_frame = price_frame.set_index("time")
+	model(df_price_frame)
 
 if __name__ == '__main__':
 	main()
