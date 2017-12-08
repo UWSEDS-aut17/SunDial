cef90c7c865a6dc68705868f1dfd02b5ee76594d
Mike Stepanovic <mrstepanovic@gmail.com>
2017-12-08 13:51:47 -0800

    optimization script for sundial model integration

--- SunDial/optimizer.py
+++ SunDial/optimizer.py
@@ -1,13 +1,27 @@
-import sdutils
+""" This program outputs the optimization window.
+"""
 
-# 
-prices = sdutils.model_prices(date, data)
-
-demand = sdutils.model_demand(date, data)
-
-output = sdutils.model_pv(data)
-
-health = sdutils.model_battery(data)
-
-
-plan = sdutils.optimize_usage(date, prices, demand, output, health)
+def optimize_solar(dt):
+  """ Does things.
+  """
+  
+  from sundial import models
+  
+  pv_hourly = models.pv_model(dt)
+  pr_hourly = models.pr_model(dt)
+  dm_hourly = models.dm_model(dt)
+  bd_hourly = models.dm_model(dt)
+  
+  sundial_df = pv_hourly.merge([pr_hourly, dm_hourly, bd_hourly])
+  
+  sundial_df['ScenarioA'] = # function for scenario A
+  sundial_df['ScenarioB'] = # function for scenario B
+  sundial_df['ScenarioC'] = # function for scenario C
+  
+def optimize_viz(df):
+  """ Does things.
+  """
+  
+  import matplotlib.pyplot as plt
+  
+  fig = 
