918f186f4d62d283ef6a21481038bc48aa7a3a98
Vardhman Mehta <mehtav@cs.washington.edu>
2017-12-11 13:30:17 -0800

    added a setup file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+from setuptools import find_packages
+
+with open(os.path.join('.', 'README.md')) as file:
+    long_description = file.read()
+
+opts = dict(
+            name="SunDial",
+            version="0.1",
+            packages=find_packages(),
+            install_requires=['pandas',
+                              'sklearn',
+                              'numpy',
+                              'datetime',
+                              ''],
+            author="Ryan Stoddard, Vardhman Mehta, Casey Pham, Michael Stepanovic, Ruchi Kwatra",
+            description="A suite of solar energy cost optimization models",
+            long_description=long_description,
+            license="MIT",
+            keywords="optimization, solar_data_analysis"
+    )
+
+if __name__ == "__main__":
+    setup(**opts)
