da1f82d600ab154abded9773b141f9e7c3ea3cc3
cdpham42 <cdpham42@gmail.com>
2017-12-11 22:32:40 -0800

    Conform to PEP8

--- sundial/pv_model/pv_model2.py
+++ sundial/pv_model/pv_model2.py
@@ -3,7 +3,8 @@ import seaborn
 import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
-import sys, os
+import sys
+import os
 from sklearn.svm import SVR
 from sklearn import neighbors
 from sklearn.model_selection import train_test_split
@@ -15,7 +16,8 @@ seaborn.set()
 
 # fetching the data
 data = pd.read_csv('sundial/data/SM_forecast_2016.csv', delimiter=',')
-solar = np.loadtxt('sundial/data/solar_data_SM_2016.csv', delimiter=',' , skiprows=1)
+solar = np.loadtxt('sundial/data/solar_data_SM_2016.csv', delimiter=',',
+                   skiprows=1)
 
 # Declaring the constants
 
@@ -25,11 +27,12 @@ tilt = 0.261799              # Panel tilt
 azi = 3.14159                # Azimuth Angle
 eff = 0.15                   # Efficiency of the panel
 
-year3 = 2016 #int(sys.argv[1])
-month3 = 2 #int(sys.argv[2])
-day3 = 7 #int(sys.argv[3])
+year3 = 2016                # int(sys.argv[1])
+month3 = 2                  # int(sys.argv[2])
+day3 = 7                    # int(sys.argv[3])
 
 # cleaning and formatting the weather data
+
 
 def clean_weather_data(data):
     times = pd.DatetimeIndex(pd.to_datetime(data['time'],
@@ -44,33 +47,58 @@ def clean_weather_data(data):
     # Dictionary for the overcast conditions
 
     """
-    {'Windy and Partly Cloudy': 1, 'Mostly Cloudy': 2, 'Breezy and Mostly Cloudy': 3, 'Breezy and Partly Cloudy': 4, 'Partly Cloudy': 12,
-    'Clear': 5, 'Overcast': 6, 'Light Rain': 7, 'Possible Light Rain': 11, 'Rain': 9, 'Breezy': 10, 'Windy': 8, 'Heavy Rain': 0, 'Windy and
-    Mostly Cloudy': 13, 'Foggy': 14, 'Breezy and Overcast': 15, 'Light Rain and Breezy': 16, 'Rain and Breezy': 17}
+    {'Windy and Partly Cloudy': 1,
+    'Mostly Cloudy': 2,
+    'Breezy and Mostly Cloudy': 3,
+    'Breezy and Partly Cloudy': 4,
+    'Partly Cloudy': 12, 'Clear': 5,
+    'Overcast': 6, 'Light Rain': 7,
+    'Possible Light Rain': 11,
+    'Rain': 9,
+    'Breezy': 10,
+    'Windy': 8,
+    'Heavy Rain': 0,
+    'Windy and Mostly Cloudy': 13,
+    'Foggy': 14,
+    'Breezy and Overcast': 15,
+    'Light Rain and Breezy': 16,
+    'Rain and Breezy': 17}
     """
     summary_values = list(set(subset["summary"]))
-    summary_dict = {value:i for (i, value) in enumerate(summary_values)}
-    subset = subset.replace({"summary" : summary_dict}).drop(subset.columns[[0, 1,10,14]],
-                                                             axis=1).fillna(0.0)
-    subset = subset.reindex_axis(['time','Date','year','month','day', 'Hour','summary','precipIntensity','precipProbability',
-                              'temperature','apparentTemperature','dewPoint','humidity','windSpeed','windBearing'
-                              ,'cloudCover','visibility'], axis=1)
+    summary_dict = {value: i for (i, value) in enumerate(summary_values)}
+    subset = subset.replace({"summary": summary_dict}).\
+        drop(subset.columns[[0, 1, 10, 14]], axis=1).fillna(0.0)
+    subset = subset.reindex_axis(['time', 'Date', 'year', 'month', 'day',
+                                  'Hour', 'summary', 'precipIntensity',
+                                  'precipProbability',
+                                  'temperature', 'apparentTemperature',
+                                  'dewPoint', 'humidity', 'windSpeed',
+                                  'windBearing', 'cloudCover',
+                                  'visibility'], axis=1)
     weather = np.array(subset)
 
     return(weather)
 
 # calculating eletcricty generated
 
+
 def get_data(solar):
 
     weather = clean_weather_data(data)
-    solar = solar[: weather.shape[0],:]
+    solar = solar[: weather.shape[0], :]
     elec = []
 
     for i in range(solar.shape[0]):
-        B = (((solar[i,6]*((np.sin(decl)*np.sin(lat)*np.cos(tilt))-(np.sin(decl)*np.cos(lat)*np.sin(tilt)*np.cos(azi))
-                        +(np.cos(decl)*np.cos(lat)*np.cos(tilt))+(np.cos(decl)*np.sin(lat)*np.sin(tilt)*np.cos(azi))
-                        +(np.cos(decl)*np.sin(tilt)*np.sin(azi)))) + ((solar[i, 5])*((1.14159-tilt)/(3.14159))))*eff*3.6)
+        B = (((solar[i, 6] * ((np.sin(decl) * np.sin(lat) * np.cos(tilt)) -
+               (np.sin(decl) * np.cos(lat) * np.sin(tilt)*np.cos(azi)) +
+               (np.cos(decl) * np.cos(lat) * np.cos(tilt))+(np.cos(decl) *
+                                                            np.sin(lat) *
+                                                            np.sin(tilt) *
+                                                            np.cos(azi)) +
+               (np.cos(decl) *
+                np.sin(tilt)*np.sin(azi)))) + ((solar[i, 5]) *
+                                               ((1.14159-tilt) /
+                                               (3.14159)))) * eff*3.6)
         elec.append(B)
 
     elec = np.vstack(np.array(elec))
@@ -80,15 +108,16 @@ def get_data(solar):
 
 # getting test-train data
 
+
 def get_data_split(year3, month3, day3):
 
     pv_output_array = get_data(solar)
 
-    year1 = pv_output_array[0,2]
-    month1 = pv_output_array[0,3]
-    day1 = pv_output_array[0,4]
+    year1 = pv_output_array[0, 2]
+    month1 = pv_output_array[0, 3]
+    day1 = pv_output_array[0, 4]
 
-    year2 = pv_output_array[0,2]
+    year2 = pv_output_array[0, 2]
     month2 = month3-1
     day2 = 30
 
@@ -96,18 +125,17 @@ def get_data_split(year3, month3, day3):
     month4 = month3
     day4 = day3
 
+    date_train_start = datetime.date(year1, month1, day1)
+    date_train_stop = datetime.date(year2, month2, day2)
+    date_test_start = datetime.date(year3, month3, day3)
+    date_test_stop = datetime.date(year4, month4, day4)
 
-
-    date_train_start = datetime.date(year1,month1,day1)
-    date_train_stop = datetime.date(year2,month2,day2)
-    date_test_start = datetime.date(year3,month3,day3)
-    date_test_stop = datetime.date(year4,month4,day4)
-
-    train = pv_output_array[(pv_output_array[:,1 ] >= (date_train_start)) & (pv_output_array[:,1 ] <= date_train_stop) &
-                            (pv_output_array[:,4] >= 0)]
-    test = pv_output_array[(pv_output_array[:,1 ] >= (date_test_start)) & (pv_output_array[:,1 ] <= date_test_stop) &
-                           (pv_output_array[:,4] >= 0)]
-
+    train = pv_output_array[(pv_output_array[:, 1] >= (date_train_start)) &
+                            (pv_output_array[:, 1] <= date_train_stop) &
+                            (pv_output_array[:, 4] >= 0)]
+    test = pv_output_array[(pv_output_array[:, 1] >= (date_test_start)) &
+                           (pv_output_array[:, 1] <= date_test_stop) &
+                           (pv_output_array[:, 4] >= 0)]
 
     x_train = train[:, 2:-1]
     y_train = train[:, -1]
@@ -118,22 +146,22 @@ def get_data_split(year3, month3, day3):
     return (x_train, y_train, x_test, y_test)
 
 
-
 def SVM_regressor():
     x_train, y_train, x_test, y_test = get_data_split(year3, month3, day3)
 
     gamma_range = [0.01, 0.001, 0.0001, 0.00001]
     epsilon_range = [x * 0.1 for x in range(0, 1)]
     C_range = (1, 500, 1000)
-    #tuned_parameters = [{'kernel': ['rbf'],'C': C_range,
-    #'gamma': gamma_range,'epsilon': epsilon_range,}]
+    # tuned_parameters = [{'kernel': ['rbf'],'C': C_range,
+    # 'gamma': gamma_range,'epsilon': epsilon_range,}]
 
     # In a perfect world, we could have enough time to optimize.
-    #svr_rbf = GridSearchCV(SVR(), param_grid=tuned_parameters, verbose=0)
+    # svr_rbf = GridSearchCV(SVR(), param_grid=tuned_parameters, verbose=0)
 
     y_svr = SVR().fit(x_train, y_train).predict(x_test)
 
     return(y_svr)
+
 
 def KNN_regressor():
     x_train, y_train, x_test, y_test = get_data_split(year3, month3, day3)
@@ -147,6 +175,8 @@ def KNN_regressor():
     return(y_knn)
 
 # choosing the best regressor
+
+
 def choose_regressor():
 
     x_train, y_train, x_test, y_test = get_data_split(year3, month3, day3)
@@ -154,12 +184,12 @@ def choose_regressor():
     y_svr = SVM_regressor()
     y_knn = KNN_regressor()
 
-    y_pred = np.empty(shape=(y_test.shape[0],1))
+    y_pred = np.empty(shape=(y_test.shape[0], 1))
 
     if (mean_squared_error(y_test, y_svr)) > mean_squared_error(y_test, y_knn):
         y_pred = y_svr
 
-    else :
+    else:
         y_pred = y_knn
 
     return(y_pred)
@@ -171,7 +201,7 @@ def plot():
     x_train, y_train, x_test, y_test = get_data_split(year3, month3, day3)
     x = range(x_test.shape[0])
 
-    plt.plot(x, y_pred,label='predicted')
+    plt.plot(x, y_pred, label='predicted')
 
     plt.legend(loc='upper right')
     plt.xlabel('Time in hours')
@@ -180,6 +210,7 @@ def plot():
     plt.savefig('pv_output.png')
     plt.show(block=True)
     print(y_pred)
+
 
 def pv_output_cph():
 
