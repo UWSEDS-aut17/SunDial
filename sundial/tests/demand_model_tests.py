1c5a7dfe5b1692ab2a462f757a0cc7f443473e38
cdpham42 <cdpham42@gmail.com>
2017-12-13 20:08:14 -0800

    Add demand tests

---
+++ sundial/tests/demand_model_tests.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 19:49:58 2017
+
+@author: Casey
+"""
+
+# %% Imports
+
+import unittest
+from sundial.demand_model import demand_model
+
+# %%
+
+class Test(unittest.TestCase):
+    """ Smoke Test """
+    
+    def Smoketest(self):
+        with self.assertRaises(Exception):
+            try:
+                demand_model.get_demand_cph()
+            except Execption:
+                pass
+            else:
+                raise Exception
+                
+    def OutputTest(self):
+        with self.assertRaises(Exception):
+            demand = demand_model.get_demand_cph()
+            self.assertTrue(len(demand) == 24)
