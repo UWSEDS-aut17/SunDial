c485c5e1f5bea1135b013835d28227f858a68c4d
Vardhman Mehta <mehtav@cs.washington.edu>
2017-12-14 12:01:17 -0800

    rectified pv_ouput errors

--- sundial/tests/pv_model_tests.py
+++ sundial/tests/pv_model_tests.py
@@ -1,8 +1,7 @@
 import os
 os.chdir('../')
 import unittest
-import numpy as np
-from sundial import pv_model
+from sundial.pv_model import pv_model
 
 
 
@@ -15,39 +14,34 @@ class UtilsTest(unittest.TestCase):
         pv_model.get_data_split(year, month, day)
 
     def test_smoke2(self):
-        saved_model = 'finalized_model.pkl'
-        y_pred = self.pv_model.save_model(saved_model)
+        saved_model = 'pv_model/finalized_model.pkl'
+        y_pred = pv_model.save_model(saved_model)
         self.assertIsNotNone(y_pred)
 
     def test_smoke3(self):
-        saved_model = 'finalized_model.pkl'
+        saved_model = 'pv_model/finalized_model.pkl'
         year = 2016
         month = 12
         day = 15
-        y_pred = self.pv_model.load_model(saved_model, year, month, day)
+        y_pred = pv_model.load_model(saved_model, year, month, day)
         self.assertTrue(len(y_pred), 24)
 
     def test_smoke4(self):
-        saved_model = 'finalized_model.pkl'
+        saved_model = 'pv_model/finalized_model.pkl'
         year = 2016
         month = 12
         day = 17
         pv_model.pv_output_cph(saved_model, year, month, day)
 
     def test_output_len(self):
-        pv_output = self.pv_model.pv_output_cph(saved_model, year, month, day)
+        saved_model = 'pv_model/finalized_model.pkl'
+        year = 2016
+        month = 12
+        day = 17
+        pv_output = pv_model.pv_output_cph(saved_model, year, month, day)
         self.assertTrue(len(pv_output), 24)
 
-    def OutputTest(self):
-        with self.assertRaises(Exception):
-            try:
-                pv_model.pv_output_cph()
-            except Exception:
-                pass
-            else:
-                raise Exception
-
-    def Output_initialization_Test(self):
+    def test_Output_initialization_Test(self):
         with self.assertRaises(Exception):
             pv_model.pv_output_cph()
 
